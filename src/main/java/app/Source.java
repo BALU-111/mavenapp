b8f41886eadd1a985dce1d778025e3a8026b3e34
Bojja Balaji <bojjabalaji2@gmail.com>
2025-11-22 16:53:49 +0530

    project added

---
+++ src/main/java/app/Source.java
@@ -0,0 +1,14 @@
+package app;
+
+import install.Utility;
+
+public class Source {
+	public static void main(String[]args) {
+               
+                System.out.println("java full stack");
+
+                System.out.println("java application");
+
+              
+}
+}
